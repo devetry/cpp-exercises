cd8433f3c0b59c747ac8a151412eec4283fe4327
Brian Schiller <bgschiller@gmail.com>
2019-09-06 11:26:02 -0600

    add some guidance for how to define Rational methods

--- 03-rational/rational.cpp
+++ 03-rational/rational.cpp
@@ -1,1 +1,10 @@
 #include "rational.hpp"
+
+// It's not allowed to declare `class Rational` again,
+// since it's already in rational.hpp. Instead, to define
+// functions as if they're inside that `class` block,
+// use the :: scoping operator. For example, you can
+// define `reduceToLowestTerms` like this:
+void Rational::reduceToLowestTerms() {
+
+}
