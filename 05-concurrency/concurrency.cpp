86981639e8b294a318d624166a2ce4348cb6c776
Jonah Jolley <jonah@devetry.com>
2019-09-10 10:33:45 -0600

    Changed to use std::all_of for test

--- 05-concurrency/concurrency.cpp
+++ 05-concurrency/concurrency.cpp
@@ -6,26 +6,25 @@
 #include <mutex>
 
 void addValue(int& accum, int i) {
- int temp = accum;
- temp += i;
- accum = temp;
+    int temp = accum;
+    temp += i;
+    accum = temp;
 }
 
 int runThreads() {
-  int accum = 0;
-  std::vector<std::thread> threads;
-  for (int i=0; i < 100; i++) {
-      // we need to pass the reference of accum to the thread
-      // but we can't pass the reference through the thread constructor
-      // wrapping accum in std:ref allows us to pass the reference
-      threads.push_back(std::thread(addValue, std::ref(accum), i));
-  }
+    int accum = 0;
+    std::vector<std::thread> threads;
+    for (int i=0; i < 100; i++) {
+        // we need to pass the reference of accum to the thread
+        // but we can't pass the reference through the thread constructor
+        // wrapping accum in std:ref allows us to pass the reference
+        threads.push_back(std::thread(addValue, std::ref(accum), i));
+    }
 
-  for (auto& th: threads) {
-      th.join();
-
-  }
-  return accum;
+    for (auto& th: threads) {
+        th.join();
+    }
+    return accum;
 }
 
 TEST_CASE( "We end up with 4950" ) {
@@ -33,7 +32,6 @@ TEST_CASE( "We end up with 4950" ) {
     for (int i=0; i< 10; i++) {
         tests.push_back(runThreads());
     }
-    REQUIRE( tests[0] == 4950);
-    REQUIRE( std::equal(tests.begin() + 1, tests.end(), tests.begin()) );
+    REQUIRE( std::all_of(tests.begin(), tests.end(), [](int i){ return i == 4950; }) );
 
 }
